6250626d4091183794e38975efd51f1acd2ae553
Zach Banks <uniMatedev@protonmail.com>
2023-07-09 11:34:33 -0700

    Update settings.py

--- banksDoorGlass/settings.py
+++ banksDoorGlass/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "convert",  # Add your "convert" app name here
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
@@ -38,6 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',ß
 ]
 
 ROOT_URLCONF = "banksDoorGlass.urls"
